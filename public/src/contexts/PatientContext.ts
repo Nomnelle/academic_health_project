494cdee1dd53594a6c561e1980b43322db06cdb8
Léa Mick <97510680+Nomnelle@users.noreply.github.com>
2025-07-26 23:36:24 +0200

    Delete public directory

--- public/src/contexts/PatientContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext, useContext } from "react";
-import type { Patient } from "../types";
-
-export const PatientContext = createContext<Patient | null>(null);
-export const usePatient = () => useContext(PatientContext);
